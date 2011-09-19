8686ac661de535a9b06486dd90205ff95bd9d0ff
Adam Young <ayoung90@live.com.au>
2011-09-19 13:41:16 +1000

    Added database poll load and question load functionality to PollApplet.

--- PollApplet/src/pollapplet/PollList.java
+++ PollApplet/src/pollapplet/PollList.java
@@ -18,20 +18,16 @@ import javax.swing.DefaultListModel;
  * 
  */
 public class PollList{
-    
-    static final String connect_string = 
-                  //"jdbc:oracle:thin:hr/hr@//localhost:1521/orcl.oracle.com";
-    "jdbc:oracle:thin:@oracle.students.itee.uq.edu.au:1521:teach, csse3004gg, groupg";
-    
-    Connection dbconn;
+
+    //Connection dbconn;
     
     /**
      * A list containing all the polls assigned to a specific Poll Master
      */
-    LinkedList<Poll> polls = new LinkedList();
+    public LinkedList<Poll> polls = new LinkedList();
 
     public PollList() {
-        
+
     }
 
     /**
@@ -42,17 +38,51 @@ public class PollList{
      * assigned to a poll.
      */
     public void loadPolls(int pollMaster){
-        // pull all polls for poll master into applet
+        Connection con;
+        try {
+            // pull all polls for poll master into applet
+            DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+            con = DriverManager.getConnection("jdbc:oracle:thin:@oracle.students.itee.uq.edu.au:1521:iteeo", "csse3004gg", "groupg");
+            
+            Statement stmt = con.createStatement();
+            ResultSet rset = stmt.executeQuery(
+            
+             "SELECT p.POLL_ID, p.POLL_NAME"
+             +" FROM POLLS P"
+             +" INNER JOIN assignedpolls a"
+             +" ON p.poll_id = a.poll_id"
+             +" WHERE user_id = "+pollMaster
+                    );
+            
+            while (rset.next()) {
+                polls.add(new Poll(Integer.parseInt(rset.getString("POLL_ID")), rset.getString("POLL_NAME")));
+            }
+            
+             for (Poll p : polls){
+                System.out.println("POLL ID: "+p.getPollId()+ " Name: " +p.getPollName()); 
+                 
+             }
+            
+            stmt.close();
+            System.out.println ("Ok.");
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PollList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }
 
  // <editor-fold defaultstate="collapsed" desc="Basic Poll class fold. Getter/Setter baby code"> 
-private class Poll {
+public class Poll {
     
     private int pollId;
     private String pollName;
 
-    public Poll(int pollId) {
+    public Poll(int pollId, String pollName) {
         this.pollId = pollId;
+        this.pollName = pollName;
     }
     public int getPollId() {
         return pollId;
@@ -74,7 +104,7 @@ public static void main(String [] args)
     {
         PollList p = new PollList();
         
-        p.loadPolls(4);
+        p.loadPolls(5);
     }
 
 }
---
+++ PollApplet/src/pollapplet/QuestionList.java
@@ -0,0 +1,154 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pollapplet;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author s4200943
+ */
+public class QuestionList {
+    
+    public LinkedList<Question> questions = new LinkedList();
+    
+    public QuestionList() {
+    }
+
+    private void loadQuestions(int pollID) {
+        Connection con;
+        try {
+            // pull all polls for poll master into applet
+            DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+            con = DriverManager.getConnection("jdbc:oracle:thin:@oracle.students.itee.uq.edu.au:1521:iteeo", "csse3004gg", "groupg");
+            
+            Statement stmt = con.createStatement();
+            ResultSet rset = stmt.executeQuery(
+             "SELECT QUESTION_ID, QUESTION_TYPE, QUESTION, CHART_STYLE, SHORT_ANSWER_TYPE, NUM"+
+             " FROM QUESTIONS Q"+
+             " WHERE Q.POLL_ID = "+pollID);      
+
+            while (rset.next()) {
+                int shorttype; 
+                        
+                if(rset.getString("SHORT_ANSWER_TYPE") == null){
+                    shorttype = -1;
+                }else{
+                    shorttype = Integer.parseInt(rset.getString("SHORT_ANSWER_TYPE"));
+                }
+                
+                
+                questions.add(new Question(
+                        Integer.parseInt(rset.getString("QUESTION_ID")), 
+                        Integer.parseInt(rset.getString("QUESTION_TYPE")),
+                        rset.getString("QUESTION"), 
+                        Integer.parseInt(rset.getString("CHART_STYLE")), 
+                        shorttype, 
+                        Integer.parseInt(rset.getString("NUM"))));
+            }
+            
+             for (Question q : questions){
+                System.out.println(
+                        "Question ID: "+q.getQuestionID()+ 
+                        " Type: " +q.getQuestionType() +
+                        " Question: "+ q.getQuestionText()+
+                        " Chart Style: "+q.getChartStyle()+
+                        " Short Answer Type: "+q.getShortAnswerType()+
+                        " Num in seq: " + q.getNumInSequence());  
+             }
+            
+            stmt.close();
+            System.out.println ("Ok.");
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PollList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+
+// <editor-fold defaultstate="collapsed" desc="Basic Question class fold. Getter/Setter baby code"> 
+public class Question {
+    
+    private int questionID;
+    private int questionType;
+    private String questionText; 
+    private int chartStyle;
+    private int shortAnswerType;
+    private int numInSequence;
+
+    public Question(int questionId, int questionType, String question, int chartStyle, int shortAnswerType, int numInSequence) {
+        this.questionID = questionId;
+        this.questionType = questionType;
+        this.questionText = question;
+        this.chartStyle = chartStyle;
+        this.shortAnswerType = shortAnswerType;
+        this.numInSequence = numInSequence;
+    }
+
+        public int getChartStyle() {
+            return chartStyle;
+        }
+
+        public void setChartStyle(int chartStyle) {
+            this.chartStyle = chartStyle;
+        }
+
+        public int getNumInSequence() {
+            return numInSequence;
+        }
+
+        public void setNumInSequence(int numInSequence) {
+            this.numInSequence = numInSequence;
+        }
+
+        public int getQuestionID() {
+            return questionID;
+        }
+
+        public void setQuestionID(int questionID) {
+            this.questionID = questionID;
+        }
+
+        public String getQuestionText() {
+            return questionText;
+        }
+
+        public void setQuestionText(String questionText) {
+            this.questionText = questionText;
+        }
+
+        public int getQuestionType() {
+            return questionType;
+        }
+
+        public void setQuestionType(int questionType) {
+            this.questionType = questionType;
+        }
+
+        public int getShortAnswerType() {
+            return shortAnswerType;
+        }
+
+        public void setShortAnswerType(int shortAnswerType) {
+            this.shortAnswerType = shortAnswerType;
+        }
+    
+}
+//</editor-fold>  
+
+
+
+public static void main(String [] args)
+    {
+        QuestionList q = new QuestionList();
+        
+        q.loadQuestions(5);
+    }
+}
